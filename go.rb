ceea2111aefef58f73bc61f44a82937377175b6b
Harman Singh <harman28@gmail.com>
2017-05-21 12:47:40 +0530

    Sends a test message

--- go.rb
+++ go.rb
@@ -1,6 +1,20 @@
 require 'dotenv'
+require 'twilio-ruby'
 
 @env = ENV['SCRAPPY_ENV'] == 'live' ? 'live' : 'test'
 Dotenv.load("environment/#{@env}.env")
 
-p ENV['twilio_sid']
+Twilio.configure do |config|
+  config.account_sid = ENV['twilio_sid']
+  config.auth_token  = ENV['twilio_token']
+end
+
+@client = Twilio::REST::Client.new
+
+@client.messages.create(
+  from: '+12536666067',
+  to:   '+919823967243',
+  body: 'Test message from Scrappy'
+)
+
+puts "Bas."
